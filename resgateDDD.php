1f841631c101bbe642fe52e8fa1cab18ba34a75a
William8400 <wdonato02@gmail.com>
2025-10-13 16:12:39 -0300

    chamando estado e a cidade

--- resgateDDD.php
+++ resgateDDD.php
@@ -1,6 +1,17 @@
 <?php
 
-echo "teste";
+$url = "https://brasilapi.com.br/api/ddd/v1/11";
 
+$resposta = file_get_contents($url);
+
+$dados = json_decode($resposta, true);
+
+$estado = $dados["state"];
+
+$cidade = $dados["cities"][0];
+
+echo "<h3>Estado: $estado </h3>";
+
+echo "<h4>Cidade: $cidade </h4>";
 
 ?>
