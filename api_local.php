12d1ffc0901b86e2315c664ad93eb8861042a2d7
William8400 <wdonato02@gmail.com>
2025-10-13 15:40:46 -0300

    fazendo os dados do JSON

--- api_local.php
+++ api_local.php
@@ -1,7 +1,15 @@
 <?php
 
-echo "teste";
+$url ="dados.json";
 
+$resposta = file_get_contents($url);
 
+$dados = json_decode($resposta, true);
+
+echo $dados['nome'];
+
+echo $dados['hobbies'][0];
+
+echo $dados['gatos']['nomes'][0];
 
 ?>
