09ef4c3b955037e1b69733b33519d92a1b9698e1
William8400 <wdonato02@gmail.com>
2025-10-20 15:16:26 -0300

    fazendo alterações

--- api.php
+++ api.php
@@ -10,8 +10,35 @@
 
 // Leitura do arquivo JSON e armazenando e transformando em Array na variável Paçocas
 $pacocas = json_decode( file_get_contents("pacoca.json"), true);
+// echo $pacocas['paçocas']['Paçoca de amendoim']['nome'];
 
-echo $pacocas['Paçoca']['Paçoca de amendoim']['nome'];
+$pacocas['paçocas']['Paçoca de Mel']['nome'] = "Paçoca de Mel";
+
+$pacocas['paçocas']['Paçoca de Mel']['tipo'] = "Doce";
+
+$pacocas['paçocas']['Paçoca de Mel']['origem'] = "Brasil";
+
+$pacocas['paçocas']['Paçoca de Mel']['nutrientes'] = "Nenhum";
+
+
+$pacocas['paçocas']['Paçoca de coco']['nome'] = "Paçoca de coco";
+
+$pacocas['paçocas']['Paçoca de coco']['tipo'] = "Doce";
+
+$pacocas['paçocas']['Paçoca de coco']['origem'] = "Belgica";
+
+$pacocas['paçocas']['Paçoca de coco']['nutrientes'] = "Cocoativo";
+
+// Salvar dados no arquivo 
+
+file_get_contents('pacoca.json', json_encode($pacocas, JSON_PRETTY_PRINT));
+
+
+
+// echo $pacocas['paçocas']['Paçoca de coco']['nome'];
+
+// echo $pacocas['paçocas']['Paçoca de Mel']['nome'];
+
 
 
 // Saída da API
