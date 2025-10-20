3ed98a4e0febddb9d152035524ca0823b4db07f0
William8400 <wdonato02@gmail.com>
2025-10-20 13:54:37 -0300

    UPDATE
    Fazendo atividade brasilapi, organizando alguns arquivos

---
+++ brasilapi.php
@@ -0,0 +1,31 @@
+<?php
+
+$url = "https://brasilapi.com.br/api/cep/v1/89010025";
+
+$resposta = file_get_contents($url);
+
+$dados = json_decode($resposta, true);
+
+$cep = $dados['cep'];
+
+$state = $dados['state'];
+
+$city = $dados['city'];
+
+$neighborhood = $dados['neighborhood'];
+
+$street = $dados['street'];
+
+$service = $dados['service'];
+
+echo "<h3> CEP: $cep </h3>";
+echo "<h3> Estado: $state </h3>";
+echo "<h3> Cidade: $street</h3>";
+echo "<h3> Vizinhança: $neighborhood </h3>";
+echo "<h3> Rua: $street </h3>";
+echo "<h3> Serviço: $service </h3>";
+
+
+
+
+
