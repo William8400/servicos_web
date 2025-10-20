b16a2c55afd718d29824bd1a741560ee09df7de8
William8400 <wdonato02@gmail.com>
2025-10-20 14:51:05 -0300

    Leitura
    do arquivo JSON e armazenando e transformando em Array na variável Paçocas

--- api.php
+++ api.php
@@ -3,13 +3,17 @@
 /* Estruturando uma API */
 
 //Cabeçalho da API 
-header("Content-type: application/json; charset=UTF-8 ");
-header("Access-Control-Allow-Origin: *");
+// header("Content-type: application/json; charset=UTF-8 ");
+// header("Access-Control-Allow-Origin: *");
 
 // Serviços do serviço web
 
-$alunos = ['nome' => 'Ana', "curso" => "design"];
+// Leitura do arquivo JSON e armazenando e transformando em Array na variável Paçocas
+$pacocas = json_decode( file_get_contents("pacoca.json"), true);
+
+echo $pacocas['Paçoca']['Paçoca de amendoim']['nome'];
+
 
 // Saída da API
 
-echo json_encode($alunos);
+// echo json_encode($alunos);
