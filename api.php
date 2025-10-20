bcf2f4fe1f718b76880ee9a5eb566f5eb9c109e2
William8400 <wdonato02@gmail.com>
2025-10-20 15:23:49 -0300

    criando uma condicional para os dados

--- api.php
+++ api.php
@@ -12,26 +12,26 @@
 $pacocas = json_decode( file_get_contents("pacoca.json"), true);
 // echo $pacocas['paçocas']['Paçoca de amendoim']['nome'];
 
-$pacocas['paçocas']['Paçoca de Mel']['nome'] = "Paçoca de Mel";
+if(false){
+    
+    $pacocas['paçocas']['Paçoca de Mel']['nome'] = "Paçoca de Mel";
 
-$pacocas['paçocas']['Paçoca de Mel']['tipo'] = "Doce";
+    $pacocas['paçocas']['Paçoca de Mel']['tipo'] = "Doce";
 
-$pacocas['paçocas']['Paçoca de Mel']['origem'] = "Brasil";
+    $pacocas['paçocas']['Paçoca de Mel']['origem'] = "Brasil";
 
-$pacocas['paçocas']['Paçoca de Mel']['nutrientes'] = "Nenhum";
+    $pacocas['paçocas']['Paçoca de Mel']['nutrientes'] = "Nenhum";
+
+}
+
+function salvardados($variavel){
+    
+    // Salvar dados no arquivo 
+    file_put_contents('pacoca.json', json_encode($variavel, JSON_PRETTY_PRINT));
+
+}
 
 
-$pacocas['paçocas']['Paçoca de coco']['nome'] = "Paçoca de coco";
-
-$pacocas['paçocas']['Paçoca de coco']['tipo'] = "Doce";
-
-$pacocas['paçocas']['Paçoca de coco']['origem'] = "Belgica";
-
-$pacocas['paçocas']['Paçoca de coco']['nutrientes'] = "Cocoativo";
-
-// Salvar dados no arquivo 
-
-file_put_contents('pacoca.json', json_encode($pacocas, JSON_PRETTY_PRINT));
 
 
 
