fd96dd2835d475417cce61eaaa342f00700999ba
William8400 <wdonato02@gmail.com>
2025-10-20 14:08:44 -0300

    fazendo a requisição url

--- cliente.php
+++ cliente.php
@@ -1,4 +1,13 @@
 <?php
 
+/* CONSUMO DE API - Utilização de um serviço web */
 
-echo "teste";
+// Requisição através de uma URL (protocolo HTTP)
+
+$url = "http://localhost/servicos-web/api.php";
+
+// guardar um valor de resposta da API 
+$resposta = file_get_contents($url);
+
+//Conversão JSON para um Array associativo 
+$valores = json_decode($resposta, true);
