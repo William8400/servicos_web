84c50c0408f7035c767745634fa75e75b35ae183
William8400 <wdonato02@gmail.com>
2025-10-23 14:10:16 -0300

    .

--- ClienteGET.php
+++ ClienteGET.php
@@ -4,7 +4,7 @@
 
 // De uma url com endpoint para requisitar algum serviço da API 
 
-$url = "http://localhost/servicos-web/apiGET.php";
+$url = "http://localhost/servicos-web/apiGET.php?pacoca=coco";
 
 $resposta = file_get_contents($url);
 
