a43acd83fef8047f71275ee7f1a83c0899d63c81
William8400 <wdonato02@gmail.com>
2025-10-23 14:01:00 -0300

    fazendo o switch

--- apiGET.php
+++ apiGET.php
@@ -6,5 +6,29 @@
 header("Content-type: application/json; charset=UTF-8 ");
 header("Access-Control-Allow-Origin: *");
 
+$metodo = $_SERVER['REQUEST_METHOD'];
+
 // Sistema API (códigos php)
 
+switch($metodo){
+
+    case "GET":
+        metodoGET();
+        break;
+
+    case "POST":
+
+        break;
+
+    default:
+        echo "Metódo não identificado";
+        break;
+
+}
+
+function metodoGET(){
+
+
+
+
+}
